5859c56e1905c31d6d3c3d0f930b8c22d4322e15
anzilabdulhakkim <anzilabdulhakkim135@gmail.com>
2024-03-04 18:57:22 +0530

    added the products api

---
+++ src/Redux/Products/Product.action.js
@@ -0,0 +1,116 @@
+import {GET_POSTS_ERROR,GET_POSTS_LOADING,GET_POSTS_SUCCESS} from "./Product.types"
+import { GET_PRODUCT_FAILURE, GET_PRODUCT_REQUEST, GET_PRODUCT_SUCCESS } from "./Product.types";
+import axios from "axios";
+import { getPersonalApi, getPostsApi, getPostsApiComputer, getPostsApiHeadphone, getPostsApiHome, getPostsApiKitchen, getPostsApiMobile, getPostsApiPersonal, getPostsApiTelevision } from "./ProductApi"
+
+export const getPosts=()=>async(dispatch)=>{
+    dispatch({type:GET_POSTS_LOADING});
+    try{
+        let data = await getPostsApi();
+        dispatch({type:GET_POSTS_SUCCESS,payload:data})
+    }catch(e){
+        dispatch({type:GET_POSTS_ERROR})
+    }
+}
+
+export const getPostsTelevisions=()=>async(dispatch)=>{
+    dispatch({type:GET_POSTS_LOADING});
+    try{
+        let data = await getPostsApiTelevision()
+        dispatch({type:GET_POSTS_SUCCESS,payload:data})
+    }catch(e){
+        dispatch({type:GET_POSTS_ERROR})
+    }
+}
+
+export const getPostsMobile=()=>async(dispatch)=>{
+    dispatch({type:GET_POSTS_LOADING});
+    try{
+        let data = await getPostsApiMobile()
+        dispatch({type:GET_POSTS_SUCCESS,payload:data})
+    }catch(e){
+        dispatch({type:GET_POSTS_ERROR})
+    }
+}
+
+export const getPostsHeadPhone=()=>async(dispatch)=>{
+    dispatch({type:GET_POSTS_LOADING});
+    try{
+        let data = await getPostsApiHeadphone()
+        dispatch({type:GET_POSTS_SUCCESS,payload:data})
+    }catch(e){
+        dispatch({type:GET_POSTS_ERROR})
+    }
+}
+
+export const getPostsHome=()=>async(dispatch)=>{
+    dispatch({type:GET_POSTS_LOADING});
+    try{
+        let data = await getPostsApiHome()
+        dispatch({type:GET_POSTS_SUCCESS,payload:data})
+    }catch(e){
+        dispatch({type:GET_POSTS_ERROR})
+    }
+}
+
+export const getPostsComputer=()=>async(dispatch)=>{
+    dispatch({type:GET_POSTS_LOADING});
+    try{
+        let data = await getPostsApiComputer()
+        dispatch({type:GET_POSTS_SUCCESS,payload:data})
+    }catch(e){
+        dispatch({type:GET_POSTS_ERROR})
+    }
+}
+
+export const getPostsKitchen=()=>async(dispatch)=>{
+    dispatch({type:GET_POSTS_LOADING});
+    try{
+        let data = await getPostsApiKitchen()
+        dispatch({type:GET_POSTS_SUCCESS,payload:data})
+    }catch(e){
+        dispatch({type:GET_POSTS_ERROR})
+    }
+}
+
+export const getPostsPersonal=()=>async(dispatch)=>{
+    dispatch({type:GET_POSTS_LOADING});
+    try{
+        let data = await getPostsApiPersonal()
+        dispatch({type:GET_POSTS_SUCCESS,payload:data})
+    }catch(e){
+        dispatch({type:GET_POSTS_ERROR})
+    }
+}
+
+export const getPersonalPost=(name,id)=>async(dispatch)=>{
+    dispatch({type:GET_POSTS_LOADING});
+    try{
+        let data = await getPersonalApi(name,id)
+        dispatch({type:GET_POSTS_SUCCESS,payload:data})
+    }catch(e){
+        dispatch({type:GET_POSTS_ERROR})
+    }
+}
+
+
+
+const getProductRequestAction = ()=>{
+    return {type:GET_PRODUCT_REQUEST}
+ }
+ 
+ const getProductSuccessAction=(payload)=>{
+     return {type:GET_PRODUCT_SUCCESS,payload}
+ }
+ 
+ const getProductFailureAction=()=>{
+     return {type:GET_PRODUCT_FAILURE}
+ }
+ 
+ export const getProduct =(paramObj)=> (dispatch)=>{
+     dispatch(getProductRequestAction())
+     axios.get("https://digital-express.vercel.app/allproduct",paramObj)
+       .then((res)=>dispatch(getProductSuccessAction(res.data)))
+       .then((res) => console.log(res.data))
+       .catch((err)=>dispatch(getProductFailureAction()))
+ }
---
+++ src/Redux/Products/ProductApi.js
@@ -0,0 +1,48 @@
+import axios from "axios"
+
+export const getPostsApi = async() => {
+    let res = await axios.get(`https://digital-express.vercel.app/allproduct`)
+    return res.data;
+}
+
+export const getPostsApiTelevision = async() => {
+    let res = await axios.get(`https://rus-digital-televisions.onrender.com/televisions`)
+    return res.data;
+}
+
+export const getPostsApiMobile = async() => {
+    let res = await axios.get(`https://rus-digital-televisions.onrender.com/mobilesandtablets`)
+    return res.data;
+}
+
+export const getPostsApiHeadphone = async() => {
+    let res = await axios.get(`https://rus-digital-televisions.onrender.com/headphones`)
+    return res.data;
+}
+
+export const getPostsApiHome = async() => {
+    let res = await axios.get(`https://rus-digital-televisions.onrender.com/homeappliances`)
+    return res.data;
+}
+
+export const getPostsApiComputer = async() => {
+    let res = await axios.get(`https://rus-digital-televisions.onrender.com/computers`)
+    return res.data;
+}
+
+
+export const getPostsApiKitchen = async() => {
+    let res = await axios.get(`https://rus-digital-televisions.onrender.com/kitchen`)
+    return res.data;
+}
+
+export const getPostsApiPersonal = async() => {
+    let res = await axios.get(`https://rus-digital-televisions.onrender.com/personalcare`)
+    return res.data;
+}
+
+export const getPersonalApi = async(name,id) => {
+    let res = await axios.get(`https://rus-digital-televisions.onrender.com/${name}/${id}`)
+    return res.data; 
+    
+}
